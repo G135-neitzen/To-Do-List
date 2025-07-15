88a6d00ffa0ffc75c1f0c8c1312c3d1c845d0d63
Giancarlo Beristain Vazquez <giancarloberistain@gmail.com>
2025-07-15 14:42:14 -0400

    Work on task.js file started

---
+++ src/task.js
@@ -0,0 +1,12 @@
+const searchButton = document.getElementById('search-tasks');
+const todos = [];
+
+class Task {
+    constructor(title, description, dueDate, priority, project) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.completed = false;
+    }
+}
